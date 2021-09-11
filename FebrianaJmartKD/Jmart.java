781434e9bd016708379282b2d84fa13786aec474
FebrianaPasonangSidauruk <febriana.pasonang@ui.ac.id>
2021-09-11 11:18:45 +0700

    modul1 Febriana Pasonang Sidauruk

--- FebrianaJmartKD/Jmart.java
+++ FebrianaJmartKD/Jmart.java
@@ -2,34 +2,71 @@ package FebrianaJmartKD;
 
 
 /**
- * Write a description of class Jmart here.
+ * class Jmart
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Febriana Pasonang Sidauruk (1906300725)
+ * @11 September 2021
  */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
+    
+    
+    public static int getPromo()
     {
-        // initialise instance variables
-        x = 0;
+       return 0;
+    }
+    
+    public static String getCustomer()
+    {
+       return "oop";
+    }
+    
+    public static float getDiscountPercentage(int before, int after)
+    {
+       if(before<after){
+        return 0.0f;
+        }
+        else{
+        float diskon = ((before - after)/before)*100;
+        return diskon;
+    }
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+   
+    public static int getDiscountedPrice(int price, float discountPercentage)
     {
-        // put your code here
-        return x + y;
+       if(discountPercentage > 100.0) {
+        discountPercentage = 100;
+        }
+        float total = price - (price*(discountPercentage/100));
+        return (int) total;
+    }
+    
+    public static int OriginalPrice(int discountedPrice, float discountPercentage)
+    {
+       int original = discountedPrice + (int) discountPercentage;
+       return original;
+    }
+    
+    public static float CommissionMultiplier()
+    {
+       return 0.05f;
+    }
+    
+    public static int getAdjustedPrice(int price)
+    {
+       int adjustedPrice = price + (int) CommissionMultiplier();
+       return adjustedPrice;
+    }
+    
+    public static int getAdminFee(int price)
+    {
+       int fee = price/2;
+       return fee;
+    }
+    
+    public static void main(String[] args)
+    {
+       
     }
 }
