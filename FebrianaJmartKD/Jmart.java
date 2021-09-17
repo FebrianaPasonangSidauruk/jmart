2b998a77a0203e7a438149792cd48637af28af6d
FebrianaPasonangSidauruk <febriana.pasonang@ui.ac.id>
2021-09-17 22:28:22 +0700

    modul1 FebrianaPasonangSidauruk perbaikan

--- FebrianaJmartKD/Jmart.java
+++ FebrianaJmartKD/Jmart.java
@@ -4,8 +4,8 @@ package FebrianaJmartKD;
 /**
  * class Jmart
  *
- * @Febriana Pasonang Sidauruk (1906300725)
- * @11 September 2021
+ * @author Febriana Pasonang Sidauruk (1906300725)
+ * @version 11 September 2021
  */
 public class Jmart
 {
@@ -27,42 +27,41 @@ public class Jmart
         return 0.0f;
         }
         else{
-        float diskon = ((before - after)/before)*100;
-        return diskon;
+        return (float) (before - after)/before * 100;
     }
     }
 
    
     public static int getDiscountedPrice(int price, float discountPercentage)
     {
-       if(discountPercentage > 100.0) {
-        discountPercentage = 100;
+       if(discountPercentage > 100.0f) {
+        return 0;
         }
+        else{
         float total = price - (price*(discountPercentage/100));
         return (int) total;
     }
-    
-    public static int OriginalPrice(int discountedPrice, float discountPercentage)
-    {
-       int original = discountedPrice + (int) discountPercentage;
-       return original;
     }
     
-    public static float CommissionMultiplier()
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+       return (int) (discountedPrice * 100.0f / (100.0f - discountPercentage));
+   }
+    
+    public static float getCommissionMultiplier()
     {
        return 0.05f;
     }
     
     public static int getAdjustedPrice(int price)
     {
-       int adjustedPrice = price + (int) CommissionMultiplier();
-       return adjustedPrice;
+       float adjustedPrice = price + getCommissionMultiplier() * price;
+       return (int) adjustedPrice;
     }
     
     public static int getAdminFee(int price)
     {
-       int fee = price/2;
-       return fee;
+       float fee = price * getCommissionMultiplier();
+       return (int) fee;
     }
     
     public static void main(String[] args)
