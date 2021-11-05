ebb43966f061373720bfa5f3a2fe0a252fc8ab10
FebrianaPasonangSidauruk <febriana.pasonang@ui.ac.id>
2021-11-05 12:48:27 +0700

    TP modul 6 Febriana Pasonang Sidauruk

--- src/FebrianaJmartKD/Jmart.java
+++ src/FebrianaJmartKD/Jmart.java
@@ -10,18 +10,45 @@ package FebrianaJmartKD;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 public class Jmart
 {
-    
+    class Country
+    {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main(String[] args)
     {
-        Complaint complaint = new Complaint (002, "Pengiriman terlalu lama");
+        String filepath = "C:/Users/FEBRIANA/jmart/jmart/src/city.json";
+        Gson gson = new Gson();
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states:");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+       /* Complaint complaint = new Complaint (002, "Pengiriman terlalu lama");
         System.out.print(complaint);
         Account account = new Account(11,"sukaBelanja","belanja@gmail.com","AyoBelanja11");
         account.validate();
         System.out.print(account);
-       System.out.println(Shipment.Duration.NEXT_DAY.getEstimatedArrival(new Date())); 
+       System.out.println(Shipment.Duration.NEXT_DAY.getEstimatedArrival(new Date())); */
     }
     
     public static Product createProduct(){
