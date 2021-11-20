1ab29ab571c5477d82edc2fabf7c42045e506e4b
FebrianaPasonangSidauruk <febriana.pasonang@ui.ac.id>
2021-11-20 11:15:24 +0700

    CS Modul8 Febriana Pasonang

--- src/main/java/com/FebrianaJmartKD/controller/PaymentController.java
+++ src/main/java/com/FebrianaJmartKD/controller/PaymentController.java
@@ -1,4 +1,43 @@
 package com.FebrianaJmartKD.controller;
+import com.FebrianaJmartKD.ObjectPoolThread;
+import com.FebrianaJmartKD.Payment;
+import com.FebrianaJmartKD.dbjson.JsonTable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class PaymentController {
+    public static final long DELIVERED_LIMIT_MS =2;
+    public static final long ON_DELIVERY_LIMIT_MS =4;
+    public static final long ON_PROGRESS_LIMIT_MS =5;
+    public static final long WAITING_CONF_LIMIT_MS =6;
+    public static JsonTable<Payment> paymentTable;
+    public static ObjectPoolThread<Payment> poolThread;
+
+    @PostMapping("/{id}/accept")
+    public boolean accept (int id){
+        return true;
+    }
+
+    @PostMapping("/{id}/cancel")
+    public boolean cancel (int id){
+        return true;
+    }
+
+    @PostMapping("/create")
+    public Payment create (int buyerId, int productId, int productCount, String shipmentAddress, byte shipmentPlan){
+        return null;
+    }
+
+    public JsonTable<Payment> getJsonTable(){
+        return null;
+    }
+
+    @PostMapping("/{id}/submit")
+    public boolean submit(int id,String receipt){
+        return true;
+    }
+
+
+    private boolean timekeeper(Payment payment){
+        return true;
+    }
 }
